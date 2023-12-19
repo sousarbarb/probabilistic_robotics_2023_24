aef4e21ff1f10bf88113f5a3dfa76118e8f148ba
Ricardo B. Sousa <sousa.ricardob@outlook.com>
2023-12-19 18:50:17 +0000

    Exercise 23b - Registration on a manifold: RBS resolution with more elegant normalizatio nbased on solution

--- src/octave/23b_registration_on_a_manifold_similiarities_norm/norm/exercise/errorAndJacobianNormRegistration.m
+++ src/octave/23b_registration_on_a_manifold_similiarities_norm/norm/exercise/errorAndJacobianNormRegistration.m
@@ -1,6 +1,7 @@
 function [e,J]=errorAndJacobianNormRegistration(X,p,z)
 
-  z_hat = normalize(X(1:3,1:3) * p + X(1:3,4));
+  p_hat = X(1:3,1:3) * p + X(1:3,4);
+  z_hat = p_hat / norm(p_hat);
   e = z_hat - z;
 
   J = zeros(3,6);
